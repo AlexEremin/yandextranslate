93c5f5a2ff6a2b7ca197d5d313a95325c4726f9b
Alex <sasha-erem@mail.ru>
2016-01-25 09:15:30 +0400

    12
    12

--- yandextranslate.rb
+++ yandextranslate.rb
@@ -8,37 +8,28 @@ class Translator
 
   URL = 'https://translate.yandex.net/api/v1.5/tr.json/'
 
-  def initialization()
-    #@con = Connect.new
-    #@req = Request.new
-    #@res = Result.new
+  def initialize()
+    @con = Connect.new
+    @req = Request.new
+    @res = Result.new
   end
 
   def detect(key, text)
-    con = Connect.new
-    req = Request.new
-    res = Result.new
-    url = req.detect(key,text)
-    get = con.connect(url)
-    puts(res.result(get))
+    url = @req.detect(key,text)
+    get = @con.connect(url)
+    puts(@res.result(get))
   end
 
   def translat(key, text, lang)
-    con = Connect.new
-    req = Request.new
-    res = Result.new
-    url = req.translate(key,text,lang)
-    get = con.connect(url)
-    puts(res.result(get))
+    url = @req.translate(key,text,lang)
+    get = @con.connect(url)
+    puts(@res.result(get))
   end
 
   def get_langs(key, ui)
-    con = Connect.new
-    req = Request.new
-    res = Result.new
-    url = req.langs(key,ui)
-    get = con.connect(url)
-    puts(res.result(get))
+    url = @req.langs(key,ui)
+    get = @con.connect(url)
+    puts(@res.result(get))
   end 
 
 end
